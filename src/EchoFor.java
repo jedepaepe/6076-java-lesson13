22bfd540b1da3122a7506c0d11cde61a8cda87ee
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-12-10 13:08:13 +0100

    exercice

--- src/EchoFor.java
+++ src/EchoFor.java
@@ -3,10 +3,9 @@ import java.util.Scanner;
 public class EchoFor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        for (; scanner.hasNext(); ) {
+        for (; true; ) {
             String text = scanner.nextLine();
             System.out.println(text);
         }
-        scanner.close();
     }
 }
