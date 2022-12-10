02d3dfc95c1abb5b84300abea3ed2d3520c98f2e
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-12-10 12:58:27 +0100

    exercice

---
+++ src/EchoSimple.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class EchoSimple {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        System.out.println(text);
+    }
+}
